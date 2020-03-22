a93e8085c325404ad0394fe68ae8ecb3edf6b4b9
guodong <tianma.123456@163.com>
2020-03-22 18:25:10 +0800

    xcode debug
    support xcode debug ldid

--- make.sh
+++ make.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-if [ -d /Applications/Xcode-5.1.1.app ]; then
-  sudo xcode-select --switch /Applications/Xcode-5.1.1.app
-fi
+#if [ -d /Applications/Xcode-5.1.1.app ]; then
+#  sudo xcode-select --switch /Applications/Xcode-5.1.1.app
+#fi
 
 if which xcrun &>/dev/null; then
     flags=(xcrun -sdk macosx g++)
-    flags+=(-mmacosx-version-min=10.4)
-
-    for arch in i386 x86_64; do
+    flags+=(-mmacosx-version-min=10.9)
+    
+    # for arch in i386 x86_64; do    // build i386 error
+    for arch in  x86_64; do
         flags+=(-arch "${arch}")
     done
 else
@@ -23,5 +24,6 @@ set -x
 "${flags[@]}" -c -std=c++11 -o ldid.o ldid.cpp
 "${flags[@]}" -o ldid ldid.o -x c lookup2.c -x c sha1.c
 
-"${flags[@]}" -c -std=c++11 -o ldid2.o ldid2.cpp
-"${flags[@]}" -o ldid2 ldid2.o -x c lookup2.c -x c sha1.c -x c sha224-256.c
+# -g flag suppor xcode debug
+"${flags[@]}" -g -c -std=c++11 -o ldid2.o ldid2.cpp
+"${flags[@]}" -g -o ldid2 ldid2.o -x c lookup2.c -x c sha1.c -x c sha224-256.c
